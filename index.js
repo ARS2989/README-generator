dcb99d212967576bee1b0aa7931b45adfd7bda65
ARS2989 <raysoloriojr@gmail.com>
2023-08-27 18:53:29 -0700

    sampleREADME change

--- index.js
+++ index.js
@@ -84,7 +84,7 @@ function writeToFile(fileName, data) {
 function init() {
   inquirer.prompt(questions).then((responses) => {
     console.log("Creating Professional README.md File...");
-    writeToFile(".MacintoshHD/Users/andressolorio/README-generator/sampleREADME", generateMarkdown({ ...responses }));
+    writeToFile("./sampleREADME", generateMarkdown({ ...responses }));
   });
 }
 init();
